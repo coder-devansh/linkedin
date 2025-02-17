2bb5999bd145421800e79352693d5f33193b0c96
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-02-17 23:11:20 +0530

    Update constant.js

--- frontened/frontend/src/utils/constant.js
+++ frontened/frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT = "https://linkedin-6v4l.vercel.app/user";
-export const JOB_API_END_POINT = "https://linkedin-6v4l.vercel.app/job";
-export const APPLICATION_API_END_POINT = "https://linkedin-6v4l.vercel.app/application";
-export const COMPANY_API_END_POINT = "https://linkedin-6v4l.vercel.app/company";
+export const USER_API_END_POINT = "https://linkedin-s122.vercel.app/user";
+export const JOB_API_END_POINT = "https://linkedin-s122.vercel.app/job";
+export const APPLICATION_API_END_POINT = "https://linkedin-s122.vercel.app/application";
+export const COMPANY_API_END_POINT = "https://linkedin-s122.vercel.app/company";
